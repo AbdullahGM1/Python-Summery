5d815466d4c2067a997aa22bdac3201b97c3877c
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-07-08 14:15:29 +0300

    Loops

--- Tests.py
+++ Tests.py
@@ -1,21 +1,9 @@
-def computepay(hour,rate):
+while True:
+    line=input("> ")
 
-    if hour<=40:
-        payment=hour*rate
-        return(payment)
-    
-    elif hour>40:
-       normal=40*rate
-       extra_hour=hour-40
-       over_time=extra_hour*1.5
-       extra_pay=over_time*rate
-       total_payment=normal+extra_pay
-       return(total_payment) 
-    
-h=input("please add you work hours: ")
-r=input("Please add your rate per hour: ")
-
-h=float(h)
-r=float(r)
-
-print(f"Your total Payment is: ${computepay(h,r)}")
+    if line[0]=="#":
+        continue
+    if line=="done":
+        break
+    print(line)
+print("Done")
