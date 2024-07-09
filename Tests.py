73c95ca2a4fe1054f53c3c9dc9ab97bbd2e12b81
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-07-09 12:56:02 +0300

    Final

--- Tests.py
+++ Tests.py
@@ -1,9 +1,38 @@
-while True:
-    line=input("> ")
 
-    if line[0]=="#":
-        continue
-    if line=="done":
-        break
-    print(line)
-print("Done")
+count=0.0
+total=0.0
+smallest=None
+largest=None
+
+while True:
+    
+    try:
+        inp=input("Please Add Numbers to get the sum, count and average > ")
+    
+        if inp=="done":
+            break
+
+        inp=int(inp)    
+        total=total+inp
+        count=count+1
+
+        if largest is None:
+            largest=inp #Changing the none to the first value
+            smallest=inp 
+
+        if inp>largest:
+            largest=inp
+        
+        if inp<smallest:
+            smallest=inp
+        
+
+    except:
+        # print("Please Just add numbers ONLY :)")
+        print("Invalid input")
+        continue # to go up again
+
+#print(f"The Sum = {total} - The Count = {count} - The Average = {total/count}")
+#print(f"The Largest Number is {largest}, and The Smallest number is {smallest}")
+print(f"Maximum is {largest}")
+print(f"Minimum is {smallest}")
